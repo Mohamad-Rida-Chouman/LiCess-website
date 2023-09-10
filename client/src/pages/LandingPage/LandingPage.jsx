eb5bbf15a37a00e71a209d4834c3568203bccb95
Mohamad-Rida-Chouman <shumanmohammadrida@gmail.com>
2023-09-10 09:24:49 +0300

    <FE> Added Landing Page Key Features Section

--- client/src/pages/LandingPage/LandingPage.jsx
+++ client/src/pages/LandingPage/LandingPage.jsx
@@ -4,8 +4,21 @@ import './LandingPage.css';
 import LandingNavbar from '../../components/LandingNavbar/LandingNavbar';
 import SvgIcon from '../../components/SvgIcon/SvgIcon';
 import ScrollDown from '../../assets/scrolldown.svg';
+import KeyFeatureCard from '../../components/KeyFeatureCard/KeyFeatureCard';
 
 const LandingPage = () => {
+	const handleDataButtonClick = () => {
+		console.log('Data Button clicked!');
+	};
+
+	const handleFeatureButtonClick = () => {
+		console.log('Feature Button clicked!');
+	};
+
+	const handleModelButtonClick = () => {
+		console.log('Model Button clicked!');
+	};
+
 	return (
 		<div className="main-landing-container">
 			<div className="hero-landing-container">
@@ -30,6 +43,39 @@ const LandingPage = () => {
 					</div>
 				</div>
 			</div>
+			<div className="briefing-landing-container flex flex-col padding-xl gap-m">
+				<div className="briefing-title-container">
+					<h2 className="briefing-title">What is LiCess?</h2>
+				</div>
+				<div className="briefing-1-container flex"></div>
+				<div className="briefing-2-container flex"></div>
+			</div>
+			<div className="key-features-landing-container flex flex-col align-center padding-xl">
+				<div className="key-features-title-container">
+					<h2 className="key-features-title">What We Provide</h2>
+				</div>
+				<div className="key-features-container flex justify-center align-center wrap gap-m padding-l">
+					<KeyFeatureCard
+						title="Data Preprocessing"
+						body="Split data into different window sizes of interest"
+						buttonText="Preprocess Data"
+						onButtonClick={handleDataButtonClick}
+					/>
+					<KeyFeatureCard
+						title="Features Extraction"
+						body="Extract features which are most common between AI models for different Post-Translational Modifications prediction"
+						buttonText="Extract Features"
+						onButtonClick={handleFeatureButtonClick}
+					/>
+					<KeyFeatureCard
+						title="S-Nitrosylation Site Prediction"
+						body="Predict S-Nitrosylation sites in proteins using machine learning models such as Light-Gradient Boosting, Extreme-Gradient Boosting..."
+						buttonText="Run Model"
+						onButtonClick={handleModelButtonClick}
+					/>
+				</div>
+			</div>
+			<div className="footer-landing-container"></div>
 		</div>
 	);
 };
