da9a5bd7f3b269cf5271adb6d111356636d0e81b
Mohamad-Rida-Chouman <shumanmohammadrida@gmail.com>
2023-09-12 01:29:02 +0300

    <FE> Updated Instructions Component

--- client/src/components/Instructions/Instructions.jsx
+++ client/src/components/Instructions/Instructions.jsx
@@ -2,8 +2,12 @@ import React from 'react';
 import './Instructions.css';
 import '../../base.css';
 
-const Instructions = ({ className, children }) => {
-	return <div className={className}>{children}</div>;
+const Instructions = ({ children }) => {
+	return (
+		<div className="instructions-container flex width-100 justify-start grey-background">
+			{children}
+		</div>
+	);
 };
 
 export default Instructions;
