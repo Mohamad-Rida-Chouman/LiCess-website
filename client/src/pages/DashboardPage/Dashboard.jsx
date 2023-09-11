8f4a0a60bafaa7257066749ea3c242cd42db8211
Mohamad-Rida-Chouman <shumanmohammadrida@gmail.com>
2023-09-12 00:11:38 +0300

    <FE> Updated Dashboard Page (Added Table)

--- client/src/pages/DashboardPage/Dashboard.jsx
+++ client/src/pages/DashboardPage/Dashboard.jsx
@@ -6,6 +6,93 @@ import Footer from '../../components/Footer/Footer';
 import PageTitle from '../../components/PageTitle/PageTitle';
 
 const Dashboard = () => {
+	const tableData = [
+		{
+			type: 'Type 1',
+			date: '2023-09-11',
+			state: 'Pending',
+			download: 'Link 1',
+			shareResult: 'Link 2',
+		},
+		{
+			type: 'Type 2',
+			date: '2023-09-12',
+			state: 'Completed',
+			download: 'Link 3',
+			shareResult: 'Link 4',
+		},
+		{
+			type: 'Type 1',
+			date: '2023-09-11',
+			state: 'Pending',
+			download: 'Link 1',
+			shareResult: 'Link 2',
+		},
+		{
+			type: 'Type 2',
+			date: '2023-09-12',
+			state: 'Completed',
+			download: 'Link 3',
+			shareResult: 'Link 4',
+		},
+		{
+			type: 'Type 1',
+			date: '2023-09-11',
+			state: 'Pending',
+			download: 'Link 1',
+			shareResult: 'Link 2',
+		},
+		{
+			type: 'Type 2',
+			date: '2023-09-12',
+			state: 'Completed',
+			download: 'Link 3',
+			shareResult: 'Link 4',
+		},
+		{
+			type: 'Type 1',
+			date: '2023-09-11',
+			state: 'Pending',
+			download: 'Link 1',
+			shareResult: 'Link 2',
+		},
+		{
+			type: 'Type 2',
+			date: '2023-09-12',
+			state: 'Completed',
+			download: 'Link 3',
+			shareResult: 'Link 4',
+		},
+		{
+			type: 'Type 1',
+			date: '2023-09-11',
+			state: 'Pending',
+			download: 'Link 1',
+			shareResult: 'Link 2',
+		},
+		{
+			type: 'Type 2',
+			date: '2023-09-12',
+			state: 'Completed',
+			download: 'Link 3',
+			shareResult: 'Link 4',
+		},
+		{
+			type: 'Type 1',
+			date: '2023-09-11',
+			state: 'Pending',
+			download: 'Link 1',
+			shareResult: 'Link 2',
+		},
+		{
+			type: 'Type 2',
+			date: '2023-09-12',
+			state: 'Completed',
+			download: 'Link 3',
+			shareResult: 'Link 4',
+		},
+	];
+
 	return (
 		<div className="dashboard-main-container width-100 flex flex-col gap-l padding-l">
 			<div className="dashboard-navbar-container">
@@ -17,14 +104,39 @@ const Dashboard = () => {
 			<div className="dashboard-subtitle">
 				<h4>Running Tasks</h4>
 			</div>
-			<div className="dashboard-content flex flex-col width-100">
-				<div className="dashboard-content-headers justify-around flex width-100 padding-s">
-					<h5>Type</h5>
-					<h5>Date</h5>
-					<h5>State</h5>
-					<h5>Download</h5>
-					<h5>Share Result</h5>
-				</div>
+			<div className="dashboard-content-table flex flex-col width-100">
+				<table className="table-content flex flex-col width-100 gap-m padding-s">
+					<thead className="width-100">
+						<tr className="flex justify-between gap-s width-100 border-underline">
+							<th className="flex justify-start width-100">Type</th>
+							<th className="flex justify-start width-100">Date</th>
+							<th className="flex justify-start width-100">State</th>
+							<th className="flex justify-start width-100">Download</th>
+							<th className="flex justify-start width-100">Share Result</th>
+						</tr>
+					</thead>
+					<tbody className="table-data flex flex-col gap-m">
+						{tableData.map((rowData, index) => (
+							<tr key={index} className="flex justify-between gap-s width-100">
+								<td className="flex justify-start align-center width-100">
+									{rowData.type}
+								</td>
+								<td className="flex justify-start align-center width-100">
+									{rowData.date}
+								</td>
+								<td className="flex justify-start align-center width-100">
+									{rowData.state}
+								</td>
+								<td className="flex justify-start align-center width-100">
+									{rowData.download}
+								</td>
+								<td className="flex justify-start align-center width-100">
+									{rowData.shareResult}
+								</td>
+							</tr>
+						))}
+					</tbody>
+				</table>
 			</div>
 			<div className="dashboard-footer-container width-100">
 				<Footer />
