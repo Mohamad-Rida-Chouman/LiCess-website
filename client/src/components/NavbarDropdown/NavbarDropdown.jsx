46efd4cedfcedc890906b9a597082f8cc5e29dd9
Mohamad-Rida-Chouman <shumanmohammadrida@gmail.com>
2023-09-12 22:24:10 +0300

    <FE> Added NavbarDropdown Component

---
+++ client/src/components/NavbarDropdown/NavbarDropdown.jsx
@@ -0,0 +1,59 @@
+import React, { useState, useEffect, useRef } from 'react';
+import './NavbarDropdown.css';
+import Button from '../Button/Button';
+import SvgIcon from '../SvgIcon/SvgIcon';
+import Hamburger from '../../assets/hamburger.svg';
+
+const NavbarDropdown = ({ options, className, dropdown }) => {
+	const dropdownRef = useRef(null);
+	const [isOpen, setIsOpen] = useState(false);
+	const toggleDropdown = () => {
+		setIsOpen(!isOpen);
+	};
+	const handleClickOutside = (event) => {
+		if (dropdownRef.current && !dropdownRef.current.contains(event.target)) {
+			setIsOpen(false);
+		}
+	};
+
+	useEffect(() => {
+		document.addEventListener('mousedown', handleClickOutside);
+		return () => {
+			document.removeEventListener('mousedown', handleClickOutside);
+		};
+	}, []);
+	return (
+		<div>
+			<Button className="button-navbar button-xs" onClick={toggleDropdown}>
+				<SvgIcon
+					className="small-icon flex align-center"
+					src={Hamburger}
+					alt="Hamburger icon"
+				/>
+				<div
+					className={`dropdown-menu ${isOpen ? 'open' : ''}`}
+					ref={dropdownRef}
+				>
+					{isOpen && (
+						<ul>
+							{options.map((option) => (
+								<li className="no-style-list" key={option.value}>
+									<Button
+										className="button-navbar button-s width-100"
+										linkTo={option.link}
+										onClick={option.click}
+									>
+										{option.label}
+									</Button>
+								</li>
+							))}
+						</ul>
+					)}
+				</div>
+			</Button>
+			{isOpen && <div className="dropdown-overlay" />}
+		</div>
+	);
+};
+
+export default NavbarDropdown;
