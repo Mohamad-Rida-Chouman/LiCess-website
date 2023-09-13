ee23b12093336226ef3a3d3e9abf2c6170d44a38
Mohamad-Rida-Chouman <shumanmohammadrida@gmail.com>
2023-09-13 15:04:58 +0300

    <FE> Added Posts to Community

--- client/src/pages/CommunityPage/Community.jsx
+++ client/src/pages/CommunityPage/Community.jsx
@@ -4,8 +4,41 @@ import '../../base.css';
 import Navbar from '../../components/Navbar/Navbar';
 import Footer from '../../components/Footer/Footer';
 import PageTitle from '../../components/PageTitle/PageTitle';
+import Post from '../../components/Post/Post';
 
 const Community = () => {
+	const postData = [
+		{
+			date: '2023-09-11',
+			email: 'user1@example.com',
+			sn: 0.85,
+			sp: 0.92,
+			acc: 0.88,
+			mcc: 0.75,
+			comment: 'This model performed exceptionally well on the test dataset.',
+			curve: <img src="path" alt="Curve 1" />,
+		},
+		{
+			date: '2023-09-12',
+			email: 'user2@example.com',
+			sn: 0.78,
+			sp: 0.95,
+			acc: 0.91,
+			mcc: 0.82,
+			comment: 'The model achieved high accuracy and specificity.',
+			curve: <img src="path" alt="Curve 2" />,
+		},
+		{
+			date: '2023-09-13',
+			email: 'user3@example.com',
+			sn: 0.92,
+			sp: 0.88,
+			acc: 0.89,
+			mcc: 0.79,
+			comment: 'The sensitivity and accuracy are noteworthy.',
+			curve: <img src="path" alt="Curve 3" />,
+		},
+	];
 	return (
 		<div className="community-main-container width-100 flex flex-col gap-l padding-l">
 			<div className="community-navbar-container">
@@ -14,7 +47,21 @@ const Community = () => {
 			<div className="community-title">
 				<PageTitle title="Community" />
 			</div>
-			<div className="community-content-container gap-s flex flex-col"></div>
+			<div className="community-content-container gap-m flex flex-col padding-l">
+				{postData.map((data, index) => (
+					<Post
+						key={index}
+						date={data.date}
+						email={data.email}
+						sn={data.sn}
+						sp={data.sp}
+						acc={data.acc}
+						mcc={data.mcc}
+						comment={data.comment}
+						curve={data.curve}
+					/>
+				))}
+			</div>
 			<div className="community-footer-container width-100">
 				<Footer />
 			</div>
