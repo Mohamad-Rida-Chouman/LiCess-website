fd531f02802a7a43407c35f5ec7ec154bc5df5a2
Mohamad-Rida-Chouman <shumanmohammadrida@gmail.com>
2023-09-13 15:31:37 +0300

    <FE> Updated Post Component (Added Footer)

--- client/src/components/Post/Post.jsx
+++ client/src/components/Post/Post.jsx
@@ -5,17 +5,20 @@ import '../../base.css';
 const Post = ({ date, email, model, sn, sp, acc, mcc, comment, curve }) => {
 	return (
 		<div className="main-post-container flex width-100 gap-m">
-			<div className="left-post-container flex flex-col width-100 gap-s">
+			<div className="left-post-container flex flex-col width-100 gap-m">
 				<div className="post-info-container flex flex-col width-100">
 					<div className="post-date">{date}</div>
 					<div className="post-owner">{email}</div>
 				</div>
 				<div className="post-content-results">
 					I would like to share with everyone the results I obtained after using
-					the {model} model: <br />
+					the <span className="bold">{model}</span> model: <br />
 					Sensitivity: {sn}, Specificity: {sp}, Accuracy: {acc}, MCC: {mcc}.
 				</div>
 				<div className="user-comment">{comment}</div>
+				<div className="post-footer">
+					For any questions, please reach to me on my email!
+				</div>
 			</div>
 			<div className="right-post-container flex justify-center align-center width-100">
 				{curve}
