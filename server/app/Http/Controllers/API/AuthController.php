4966dc2ae3f3acc0d8b0bc03f47d1ab6d8269624
Mohamad-Rida-Chouman <shumanmohammadrida@gmail.com>
2023-09-16 23:06:23 +0300

    <BE> Fixed Auth Controller

--- server/app/Http/Controllers/API/AuthController.php
+++ server/app/Http/Controllers/API/AuthController.php
@@ -43,13 +43,13 @@ class AuthController extends Controller
     public function register(Request $request)
     {
         $request->validate([
-            'name' => 'required|string|max:255',
+            'username' => 'required|string|max:255',
             'email' => 'required|string|email|max:255|unique:users',
             'password' => 'required|string|min:6',
         ]);
 
         $user = User::create([
-            'name' => $request->name,
+            'username' => $request->username,
             'email' => $request->email,
             'password' => Hash::make($request->password),
         ]);
