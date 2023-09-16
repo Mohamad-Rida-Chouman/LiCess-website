bd0466f5f072384392f729037241e4e95bc3e92a
Mohamad-Rida-Chouman <shumanmohammadrida@gmail.com>
2023-09-16 23:06:45 +0300

    <BE> Fixed User Model

--- server/app/Models/User.php
+++ server/app/Models/User.php
@@ -19,7 +19,7 @@ class User extends Authenticatable implements JWTSubject
      * @var array<int, string>
      */
     protected $fillable = [
-        'name',
+        'username',
         'email',
         'password',
     ];
