87117bc50fc34e1c6050f65ab0743918d64aed19
Mohamad-Rida-Chouman <shumanmohammadrida@gmail.com>
2023-09-16 23:14:50 +0300

    <BE> Updated User Model (MustVerifyEmail)

--- server/app/Models/User.php
+++ server/app/Models/User.php
@@ -9,7 +9,7 @@ use PHPOpenSourceSaver\JWTAuth\Contracts\JWTSubject;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 
-class User extends Authenticatable implements JWTSubject
+class User extends Authenticatable implements JWTSubject, MustVerifyEmail
 {
     use HasApiTokens, HasFactory, Notifiable;
 
