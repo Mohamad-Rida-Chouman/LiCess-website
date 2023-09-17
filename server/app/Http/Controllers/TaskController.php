8d8941e4ceafebd4b67775ea3761c6b834c1e757
Mohamad-Rida-Chouman <shumanmohammadrida@gmail.com>
2023-09-17 21:13:52 +0300

    <BE> Updated Task Controller

--- server/app/Http/Controllers/TaskController.php
+++ server/app/Http/Controllers/TaskController.php
@@ -3,8 +3,63 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class TaskController extends Controller
 {
-    //
+    public function index()
+    {
+        $tasks = Task::all();
+        return response()->json($tasks);
+    }
+
+    public function create()
+    {
+        //
+    }
+
+    public function store(Request $request)
+    {
+        $validator = Validator::make( $request->all(), [
+            'user_id' => 'required|number',
+            'task_name' => 'required|string',
+            'date' => 'required|date',
+            'state' => 'required|string',
+        ]);
+
+        if ( $validator->fails() ) {
+            return response()->json($validator->errors(), 500);
+        }
+
+        $task = Task::create($request->all());
+        return response()->json($task, 200);
+    }
+
+    public function show(Task $task)
+    {
+        return response()->json($task, 200);
+    }
+
+    public function update(Request $request, Task $task)
+    {
+        $validator = Validator::make( $request->all(), [
+            'user_id' => 'required|number',
+            'task_name' => 'required|string',
+            'date' => 'required|date',
+            'state' => 'required|string',
+        ]);
+
+        if ( $validator->fails() ) {
+            return response()->json($validator->errors(), 500);
+        }
+
+        $task = Task::create($request->all());
+        return response()->json($task, 200);
+    }
+
+    public function destroy(Task $task)
+    {
+        $task->delete();
+        return response()->json('',200);
+    }
 }
