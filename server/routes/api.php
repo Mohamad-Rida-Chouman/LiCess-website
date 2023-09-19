3c8afc18d6952fd4e990a2ac1c9542010e228c72
Mohamad-Rida-Chouman <shumanmohammadrida@gmail.com>
2023-09-19 11:45:34 +0300

    <BE> Added Preprocess Route

--- server/routes/api.php
+++ server/routes/api.php
@@ -26,6 +26,7 @@ Route::controller(AuthController::class)->group(function () {
 
 Route::resource('tasks', TaskController::class);
 Route::post('tasks/{id}', [TaskController::class, 'update']);
+Route::post('preprocess', [TaskController::class, 'createPreprocessingTask']);
 
 
 // Route::get('/email/verify', function () {
