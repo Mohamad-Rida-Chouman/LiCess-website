21fea3747db3687241113034fc75fb48d29999c0
Mohamad-Rida-Chouman <shumanmohammadrida@gmail.com>
2023-09-19 11:53:53 +0300

    <BE> Updated Result Model (Fillables)

--- server/app/Models/Result.php
+++ server/app/Models/Result.php
@@ -8,4 +8,12 @@ use Illuminate\Database\Eloquent\Model;
 class Result extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'task_id',
+        'task_type',
+        'label',
+        'data'
+    ];
+
 }
