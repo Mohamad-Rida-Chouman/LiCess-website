5ec043c0d312b53308e63b360aa16091409a8f0a
Mohamad-Rida-Chouman <shumanmohammadrida@gmail.com>
2023-09-20 21:04:18 +0300

    <FE> Updated React Select Component

--- client/src/components/ReactSelectDropdown/ReactSelectDropdown.jsx
+++ client/src/components/ReactSelectDropdown/ReactSelectDropdown.jsx
@@ -1,18 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../../base.css';
 import Select from 'react-select';
-import makeAnimated from 'react-select/animated';
+import Button from '../Button/Button';
 
-const animatedComponents = makeAnimated();
+const ReactSelectDropdown = (props) => {
+	const [selectedOptions, setSelectedOptions] = useState();
 
-const ReactSelectDropdown = (defaultValue, dropdownOptions) => {
-	<Select
-		closeMenuOnSelect={false}
-		components={animatedComponents}
-		defaultValue={defaultValue}
-		isMulti
-		options={dropdownOptions}
-	/>;
+	function handleSelect(data) {
+		setSelectedOptions(data);
+	}
+
+	return (
+		<div className="main-dropdown-container">
+			<div className="dropdown-container">
+				<Select
+					closeMenuOnSelect={false}
+					options={props.optionList}
+					placeholder="Select Window Size(s)"
+					value={selectedOptions}
+					onChange={handleSelect}
+					isSearchable={true}
+					isMulti
+				/>
+			</div>
+		</div>
+	);
 };
 
 export default ReactSelectDropdown;
