b16bb05f924af440ed53f8156ff42b74865d86d0
Mohamad-Rida-Chouman <shumanmohammadrida@gmail.com>
2023-09-21 23:22:14 +0300

    <BE> Updated Mail Controller

--- server/app/Http/Controllers/MailController.php
+++ server/app/Http/Controllers/MailController.php
@@ -3,8 +3,22 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Mail;
+use App\Mail\DemoMail;
 
 class MailController extends Controller
 {
-    //
+    public function index()
+    {
+        $mailData = [
+            'title' => 'Mail from LiCess-Project',
+            'body' => 'Thank you for using LiCess.
+            Your results are ready.
+            Please click on the button below to move to the dashboard and be amazed!',
+        ];
+
+        Mail::to('jaden.yuki.jy1@gmail.com')->send(new DemoMail($mailData));
+
+        dd('Email send successfully.');
+    }
 }
