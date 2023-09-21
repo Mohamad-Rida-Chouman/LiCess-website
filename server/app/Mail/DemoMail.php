c54a2613d547b97b645d082b578b5efdec36c0be
Mohamad-Rida-Chouman <shumanmohammadrida@gmail.com>
2023-09-21 23:32:14 +0300

    <BE> Updated Demo Mail Title

--- server/app/Mail/DemoMail.php
+++ server/app/Mail/DemoMail.php
@@ -28,7 +28,7 @@ class DemoMail extends Mailable
     public function envelope(): Envelope
     {
         return new Envelope(
-            subject: 'Demo Mail',
+            subject: 'Results From LiCess',
         );
     }
 
