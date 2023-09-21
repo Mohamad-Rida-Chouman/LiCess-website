c52181913b5a42133b771d77f0fa630ed19d81b2
Mohamad-Rida-Chouman <shumanmohammadrida@gmail.com>
2023-09-21 23:32:50 +0300

    <BE> Added Demo Mail Blade

---
+++ server/resources/views/emails/demoMail.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Webappfix</title>
+</head>
+<body>
+    <h1>{{ $mailData['title'] }}</h1>
+
+    <p>{{ $mailData['body'] }}</p>
+
+    <p> There will be a button here soon ;)</p>
+
+    <p>Stay Tuned</p>
+
+</body>
+</html>
