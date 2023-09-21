41704331bfef23daa671487b621552e47a930d0d
Mohamad-Rida-Chouman <shumanmohammadrida@gmail.com>
2023-09-21 23:18:07 +0300

    <BE> Updated Mail Class

--- server/app/Mail/DemoMail.php
+++ server/app/Mail/DemoMail.php
@@ -13,12 +13,13 @@ class DemoMail extends Mailable
 {
     use Queueable, SerializesModels;
 
+    public $mailData;
     /**
      * Create a new message instance.
      */
-    public function __construct()
+    public function __construct($mailData)
     {
-        //
+        $this->mailData = $mailData;
     }
 
     /**
@@ -37,14 +38,14 @@ class DemoMail extends Mailable
     public function content(): Content
     {
         return new Content(
-            view: 'view.name',
+            view: 'emails.demoMail',
         );
     }
 
     /**
      * Get the attachments for the message.
      *
-     * @return array<int, \Illuminate\Mail\Mailables\Attachment>
+     * @return array
      */
     public function attachments(): array
     {
