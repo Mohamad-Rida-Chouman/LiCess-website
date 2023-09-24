471668694afa0544af61bff42d55fac936edb78b
Mohamad-Rida-Chouman <shumanmohammadrida@gmail.com>
2023-09-24 16:20:43 +0300

    <FE> Updated Instructions Style (Flex Column)

--- client/src/components/Instructions/Instructions.jsx
+++ client/src/components/Instructions/Instructions.jsx
@@ -4,7 +4,7 @@ import '../../base.css';
 
 const Instructions = ({ children }) => {
 	return (
-		<div className="instructions-container flex width-100 justify-start grey-background">
+		<div className="instructions-container flex flex-col width-100 justify-start grey-background">
 			{children}
 		</div>
 	);
