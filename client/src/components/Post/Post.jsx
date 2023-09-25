1071950bfc04dcc79fef953e1aad0c18bda2bf3e
Mohamad-Rida-Chouman <shumanmohammadrida@gmail.com>
2023-09-25 20:28:41 +0300

    <FE> Modified Post (Removed Textarea)

--- client/src/components/Post/Post.jsx
+++ client/src/components/Post/Post.jsx
@@ -71,37 +71,28 @@ const Post = ({ date, email, model, sn, sp, acc, mcc, auc, fpr, tpr }) => {
 	};
 
 	return (
-		<div className="whole-post-container border">
-			<textarea
-				name="comment"
-				id="comment"
-				wrap="soft"
-				placeholder="Share your thoughts!"
-				className="user-comment width-100 padding-s"
-			></textarea>
-			<div className="main-post-container flex width-100 gap-m padding-s">
-				<div className="left-post-container flex flex-col width-100 gap-m">
-					<div className="post-info-container flex flex-col width-100">
-						<div className="post-date notice-font">{date}</div>
-						<div className="post-owner notice-font">{email}</div>
-					</div>
-					<div className="post-content-results">
-						I would like to share with everyone the results I obtained after
-						using the <span className="bold">{model}</span> model: <br />
-						<span className="bold">Accuracy:</span> {accuracy},<br />
-						<span className="bold">Sensitivity:</span> {sensitivity},<br />
-						<span className="bold">Specificity:</span> {Specificity},<br />
-						<span className="bold">AUC:</span> {AUC},<br />
-						<span className="bold">MCC:</span> {MCC}.
-					</div>
-					<div className="post-footer">
-						For any questions, please reach to me on my email!
-					</div>
+		<div className="main-post-container flex width-100 gap-m padding-s">
+			<div className="left-post-container flex flex-col width-100 gap-m">
+				<div className="post-info-container flex flex-col width-100">
+					<div className="post-date notice-font">{date}</div>
+					<div className="post-owner notice-font">{email}</div>
 				</div>
-				<div className="right-post-container flex justify-center align-center">
-					<div className="curve-container flex justify-center align-center">
-						<Line options={options} data={data} />
-					</div>
+				<div className="post-content-results">
+					I would like to share with everyone the results I obtained after using
+					the <span className="bold">{model}</span> model: <br />
+					<span className="bold">Accuracy:</span> {accuracy},<br />
+					<span className="bold">Sensitivity:</span> {sensitivity},<br />
+					<span className="bold">Specificity:</span> {Specificity},<br />
+					<span className="bold">AUC:</span> {AUC},<br />
+					<span className="bold">MCC:</span> {MCC}.
+				</div>
+				<div className="post-footer">
+					For any questions, please reach to me on my email!
+				</div>
+			</div>
+			<div className="right-post-container flex justify-center align-center">
+				<div className="curve-container flex justify-center align-center">
+					<Line options={options} data={data} />
 				</div>
 			</div>
 		</div>
