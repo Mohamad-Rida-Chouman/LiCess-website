ac91fbb2f21cbad672e8332cc55155e40a69a2a9
Mohamad-Rida-Chouman <shumanmohammadrida@gmail.com>
2023-09-25 19:45:09 +0300

    <BE> Updated Post Controller

--- server/app/Http/Controllers/PostController.php
+++ server/app/Http/Controllers/PostController.php
@@ -2,9 +2,60 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Post;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class PostController extends Controller
 {
-    //
+    public function index()
+    {
+        $post = Post::all();
+        return response()->json($post);
+    }
+
+    public function create()
+    {
+        //
+    }
+
+    public function store(Request $request)
+    {
+        $validator = Validator::make( $request->all(), [
+            'user_id' => 'required|numeric',
+            'date' => 'required|date',
+            'user_email' => 'required|email',
+            'sensitivity' => 'required|numeric',
+            'specificity' => 'required|numeric',
+            'acc' => 'required|numeric',
+            'mcc' => 'required|numeric',
+            'auc' => 'required|numeric',
+            'fpr' => 'required|string',
+            'tpr' => 'required|string',
+            'comment' => 'required|string',
+        ]);
+
+        if ( $validator->fails() ) {
+            return response()->json($validator->errors(), 500);
+        }
+
+        $post = Post::create($request->all());
+        return response()->json($post, 200);
+    }
+
+    public function show(Post $post)
+    {
+        return response()->json($post, 200);
+    }
+
+    public function update()
+    {
+        //
+    }
+
+    public function destroy(Post $post)
+    {
+        $post->delete();
+        return response()->json('',200);
+    }
 }
