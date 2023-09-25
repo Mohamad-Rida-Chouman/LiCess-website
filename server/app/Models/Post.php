7fcf98b792644010bd4c9f1453d232f16715f303
Mohamad-Rida-Chouman <shumanmohammadrida@gmail.com>
2023-09-25 19:40:58 +0300

    <BE> Updated Post Model

--- server/app/Models/Post.php
+++ server/app/Models/Post.php
@@ -8,4 +8,18 @@ use Illuminate\Database\Eloquent\Model;
 class Post extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'user_id',
+        'date',
+        'user_email',
+        'sensitivity',
+        'specificity',
+        'acc',
+        'mcc',
+        'auc',
+        'fpr',
+        'tpr',
+        'comment',
+    ];
 }
