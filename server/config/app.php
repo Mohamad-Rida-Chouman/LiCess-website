d33c60b759cd7288f3a204f073a3bbed199dbd0a
Mohamad-Rida-Chouman <shumanmohammadrida@gmail.com>
2023-09-26 10:27:08 +0300

    <BE> Added JWT-Related Commands

--- server/config/app.php
+++ server/config/app.php
@@ -163,9 +163,9 @@ return [
         /*
          * Application Service Providers...
          */
+        
+        Tymon\JWTAuth\Providers\LaravelServiceProvider::class,
         App\Providers\AppServiceProvider::class,
-        App\Providers\AuthServiceProvider::class,
-        // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
     ])->toArray(),
@@ -183,6 +183,8 @@ return [
 
     'aliases' => Facade::defaultAliases()->merge([
         // 'Example' => App\Facades\Example::class,
+        'JWTAuth' => Tymon\JWTAuth\Facades\JWTAuth::class,
+        'JWTFactory' => Tymon\JWTAuth\Facades\JWTFactory::class,
     ])->toArray(),
 
 ];
