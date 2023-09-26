a1eb40c974e82d28ee616e0c657fadb4cc945adb
Mohamad-Rida-Chouman <shumanmohammadrida@gmail.com>
2023-09-26 21:38:13 +0300

    <FE> Created Random Fasta Demo Downloader

--- client/src/components/DemoFasta/DemoFasta.jsx
+++ client/src/components/DemoFasta/DemoFasta.jsx
@@ -1,10 +1,36 @@
 import React from 'react';
 import Button from '../Button/Button';
 import '../../base.css';
+import { saveAs } from 'file-saver';
+import Fasta from '../../assets/Proteins.fasta';
 
 const DemoFasta = () => {
+	const fastaFile = Fasta;
+
 	const downloadDemo = async () => {
-		console.log('download pressed');
+		fetch(fastaFile)
+			.then(function (response) {
+				return response.text();
+			})
+			.then(function (data) {
+				var n = 500;
+				var matches = data.match(/>sp\|.*\|.*\n[^>]+/g);
+				function shuffleArray(array) {
+					for (let i = array.length - 1; i > 0; i--) {
+						const j = Math.floor(Math.random() * (i + 1));
+						[array[i], array[j]] = [array[j], array[i]];
+					}
+				}
+				shuffleArray(matches);
+				var selectedProteins = matches.slice(0, n);
+				var newProteinString = selectedProteins.join('\n');
+
+				const blob = new Blob([newProteinString], {
+					type: 'text/plain;charset=utf-8',
+				});
+
+				saveAs(blob, 'proteins_demo' + '.fasta');
+			});
 	};
 	return (
 		<div>
