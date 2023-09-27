bd6d7ea4b19c7d981a8575c343417239844f553b
Mohamad-Rida-Chouman <shumanmohammadrida@gmail.com>
2023-09-27 12:55:12 +0300

    <FE> Added PieCharts to Admin Page

--- client/src/pages/AdminPage/AdminPage.jsx
+++ client/src/pages/AdminPage/AdminPage.jsx
@@ -1,7 +1,20 @@
+import PieChartComp from '../../components/PieChartComp/PieChartComp';
+import './AdminPage.css';
+import '../../base.css';
+
 const AdminPage = () => {
+	const pieData1 = [
+		{ name: 'name1', value: 400 },
+		{ name: 'name2', value: 700 },
+		{ name: 'name3', value: 200 },
+		{ name: 'name4', value: 1000 },
+	];
 	return (
-		<div>
-			<div>hi</div>
+		<div className="pie-container flex justify-center align-center flex-wrap">
+			<PieChartComp data={pieData1}></PieChartComp>
+			<PieChartComp data={pieData1}></PieChartComp>
+			<PieChartComp data={pieData1}></PieChartComp>
+			<PieChartComp data={pieData1}></PieChartComp>
 		</div>
 	);
 };
