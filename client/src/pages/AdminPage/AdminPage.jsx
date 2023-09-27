f4a1ccfd60868b7427669e4cd54a4441dadfdcf8
Mohamad-Rida-Chouman <shumanmohammadrida@gmail.com>
2023-09-27 13:27:44 +0300

    <FE> Added Linechart Component to Admin Page

--- client/src/pages/AdminPage/AdminPage.jsx
+++ client/src/pages/AdminPage/AdminPage.jsx
@@ -1,6 +1,8 @@
 import PieChartComp from '../../components/PieChartComp/PieChartComp';
 import './AdminPage.css';
 import '../../base.css';
+import LineChartComp from '../../components/LineChartComp/LineChartComp';
+import PageTitle from '../../components/PageTitle/PageTitle';
 
 const AdminPage = () => {
 	const pieData1 = [
@@ -9,12 +11,34 @@ const AdminPage = () => {
 		{ name: 'name3', value: 200 },
 		{ name: 'name4', value: 1000 },
 	];
+	const lineData1 = [
+		{ name: 'Monday', value: 11 },
+		{ name: 'Tuesday', value: 15 },
+		{ name: 'Wednesday', value: 8 },
+		{ name: 'Thursday', value: 8 },
+		{ name: 'Friday', value: 5 },
+		{ name: 'Saturday', value: 2 },
+		{ name: 'Sunday', value: 0 },
+	];
 	return (
-		<div className="pie-container flex justify-center align-center flex-wrap">
-			<PieChartComp data={pieData1}></PieChartComp>
-			<PieChartComp data={pieData1}></PieChartComp>
-			<PieChartComp data={pieData1}></PieChartComp>
-			<PieChartComp data={pieData1}></PieChartComp>
+		<div className="admin-main-container flex flex-col gap-l padding-s">
+			<div className="piecharts-container flex justify-center align-center flex-wrap gap-s">
+				<div className="piechart-container">
+					<PieChartComp data={pieData1}></PieChartComp>
+				</div>
+				<div className="piechart-container">
+					<PieChartComp data={pieData1}></PieChartComp>
+				</div>
+				<div className="piechart-container">
+					<PieChartComp data={pieData1}></PieChartComp>
+				</div>
+				<div className="piechart-container">
+					<PieChartComp data={pieData1}></PieChartComp>
+				</div>
+			</div>
+			<div className="linecharts-container flex justify-center align-center flex-wrap padding-s">
+				<LineChartComp data={lineData1}></LineChartComp>
+			</div>
 		</div>
 	);
 };
