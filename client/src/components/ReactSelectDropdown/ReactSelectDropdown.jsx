3ba7c8892841b4481db683d98cd66b608afc9d30
Mohamad-Rida-Chouman <shumanmohammadrida@gmail.com>
2023-09-27 08:36:41 +0300

    <FE> Added New React Select Style

--- client/src/components/ReactSelectDropdown/ReactSelectDropdown.jsx
+++ client/src/components/ReactSelectDropdown/ReactSelectDropdown.jsx
@@ -11,6 +11,59 @@ const ReactSelectDropdown = (props) => {
 		props.parentCallback(data);
 	}
 
+	const customStyles = {
+		option: (defaultStyles, state) => ({
+			...defaultStyles,
+			color: state.isSelected ? '#ffffff' : '#4b97e8',
+			backgroundColor: state.isSelected ? '#4b97e8' : '#ffffff',
+		}),
+
+		control: (defaultStyles) => ({
+			...defaultStyles,
+			backgroundColor: '#4b97e8',
+			padding: '0.5px',
+			border: 'none',
+			borderRadius: '5px',
+			boxShadow: 'none',
+			minHeight: '0px',
+			cursor: 'pointer',
+		}),
+		singleValue: (defaultStyles) => ({ ...defaultStyles, color: '#ffffff' }),
+		container: (defaultStyles) => ({
+			...defaultStyles,
+			padding: '0px',
+			minHeight: '0px',
+			borderRadius: '5px',
+			cursor: 'pointer',
+		}),
+		placeholder: (defaultStyles) => ({ ...defaultStyles, color: '#ffffff' }),
+		valueContainer: (defaultStyles) => ({ ...defaultStyles, color: '#ffffff' }),
+		clearIndicator: (defaultStyles) => ({ ...defaultStyles, color: '#ffffff' }),
+		input: (defaultStyles) => ({ ...defaultStyles, color: '#ffffff' }),
+		dropdownIndicator: (defaultStyles) => ({
+			...defaultStyles,
+			color: '#ffffff',
+		}),
+		menu: (defaultStyles) => ({
+			...defaultStyles,
+			padding: '0px',
+			minHeight: '0px',
+			borderRadius: '5px',
+		}),
+		menuList: (defaultStyles) => ({
+			...defaultStyles,
+			maxHeight: '36vh',
+			scrollbarColor: 'aliceblue aliceblue',
+			borderRadius: '5px',
+		}),
+		scrollbar: (defaultStyles) => ({
+			...defaultStyles,
+			maxHeight: '36vh',
+			color: 'aliceblue',
+			borderRadius: '5px',
+		}),
+	};
+
 	return (
 		<div className="main-dropdown-container">
 			<div className="dropdown-container">
@@ -23,6 +76,7 @@ const ReactSelectDropdown = (props) => {
 					onChange={handleSelect}
 					isSearchable={true}
 					isMulti
+					styles={customStyles}
 				/>
 			</div>
 		</div>
