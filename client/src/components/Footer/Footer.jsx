a0a003911f2a561c12dcf6d8a259ff36416a5075
Mohamad-Rida-Chouman <shumanmohammadrida@gmail.com>
2023-09-27 18:04:01 +0300

    <FE> Added Links to Icons (Footer)

--- client/src/components/Footer/Footer.jsx
+++ client/src/components/Footer/Footer.jsx
@@ -11,9 +11,19 @@ const Footer = () => {
 		<div className="main-footer-container flex align-center justify-around width-100 padding-l">
 			<div className="left-footer-container flex">Copyrights</div>
 			<div className="right-footer-container flex gap-s">
-				<SvgIcon className="small-icon" src={EmailSvg} alt="email icon" />
-				<SvgIcon className="small-icon" src={GithubSvg} alt="github icon" />
-				<SvgIcon className="small-icon" src={LinkedInSvg} alt="linkedin icon" />
+				<a href="mailto:shumanmohammadrida@gmail.com">
+					<SvgIcon className="small-icon" src={EmailSvg} alt="email icon" />
+				</a>
+				<a href="https://github.com/Mohamad-Rida-Chouman/">
+					<SvgIcon className="small-icon" src={GithubSvg} alt="github icon" />
+				</a>
+				<a href="https://www.linkedin.com/in/mohamad-rida-chouman/">
+					<SvgIcon
+						className="small-icon"
+						src={LinkedInSvg}
+						alt="linkedin icon"
+					/>
+				</a>
 			</div>
 		</div>
 	);
