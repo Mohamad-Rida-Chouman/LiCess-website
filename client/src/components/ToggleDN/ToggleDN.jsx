8706b1ebd7a0bc4a7054c6314794dbefe3e65708
Mohamad-Rida-Chouman <shumanmohammadrida@gmail.com>
2023-09-27 19:47:34 +0300

    <FE> Added Toggle Day/Night Functionalities

--- client/src/components/ToggleDN/ToggleDN.jsx
+++ client/src/components/ToggleDN/ToggleDN.jsx
@@ -1,9 +1,54 @@
+import React, { useEffect } from 'react';
 import './ToggleDN.css';
+
 const ToggleDN = () => {
+	useEffect(() => {
+		const cookie = checkCookie('Licess-day');
+		if (!cookie) {
+			setChecked();
+		} else {
+			changeColorsDay();
+		}
+	}, []);
+
+	const setChecked = () => {
+		document.getElementById('checkbox').checked = true;
+	};
+	const changeColorsDay = () => {
+		document.body.style.backgroundColor = '#ffffff';
+		document.body.style.color = '#020d17';
+	};
+	const changeColorsNight = () => {
+		document.body.style.backgroundColor = '#020d17';
+		document.body.style.color = '#ffffff';
+	};
+
+	const checkCookie = (name) => {
+		return document.cookie
+			.split(';')
+			.some((item) => item.trim().startsWith(`${name}=`));
+	};
+
+	const toggleColors = (e) => {
+		if (e.target.checked) {
+			document.cookie =
+				'Licess-day=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;';
+			changeColorsNight();
+		} else {
+			document.cookie = 'Licess-day=true';
+			changeColorsDay();
+		}
+		console.log(e.target.checked);
+	};
 	return (
 		<div className="toggle-button">
 			<label className="bb8-toggle">
-				<input className="bb8-toggle__checkbox" type="checkbox" />
+				<input
+					className="bb8-toggle__checkbox"
+					id="checkbox"
+					type="checkbox"
+					onChange={toggleColors}
+				/>
 				<div className="bb8-toggle__container">
 					<div className="bb8-toggle__scenery">
 						<div className="bb8-toggle__star"></div>
