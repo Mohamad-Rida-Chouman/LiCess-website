5a50a59fef1f5175fdd7bc67322e97db88d8f82f
Mohamad-Rida-Chouman <shumanmohammadrida@gmail.com>
2023-09-27 16:08:39 +0300

    <FE> Updated Data Input For Piecharts

--- client/src/pages/AdminPage/AdminPage.jsx
+++ client/src/pages/AdminPage/AdminPage.jsx
@@ -152,34 +152,6 @@ const AdminPage = () => {
 			});
 	}
 
-	const pieData1 = [
-		{ name: 'Data Preprocessing', value: 400 },
-		{ name: 'Feature Extraction', value: 700 },
-		{ name: 'Model Run', value: 200 },
-	];
-	const pieData2 = [
-		{ name: 'LGB', value: 400 },
-		{ name: 'XGB', value: 300 },
-		{ name: 'RF', value: 50 },
-		{ name: 'Ensemble', value: 500 },
-	];
-	const pieData3 = [
-		{ name: 'AAC', value: 50 },
-		{ name: 'AAI', value: 60 },
-		{ name: 'Binary', value: 20 },
-		{ name: 'QSO', value: 30 },
-		{ name: 'APAAC', value: 40 },
-	];
-
-	const lineData1 = [
-		{ name: 'Monday', tasks: 11 },
-		{ name: 'Tuesday', tasks: 15 },
-		{ name: 'Wednesday', tasks: 8 },
-		{ name: 'Thursday', tasks: 8 },
-		{ name: 'Friday', tasks: 5 },
-		{ name: 'Saturday', tasks: 2 },
-		{ name: 'Sunday', tasks: 0 },
-	];
 	return (
 		<div className="admin-main-container flex flex-col gap-l padding-s width-90">
 			<div className="flex">
@@ -189,18 +161,24 @@ const AdminPage = () => {
 				</Button>
 			</div>
 			<div className="piecharts-container flex justify-between align-center flex-wrap gap-s">
-				<div className="piechart-container flex flex-col align-center justify-center">
-					<h2>Tasks Usage</h2>
-					<PieChartComp data={pieData1}></PieChartComp>
-				</div>
-				<div className="piechart-container flex flex-col align-center justify-center">
-					<h2>Models Usage</h2>
-					<PieChartComp data={pieData2}></PieChartComp>
-				</div>
-				<div className="piechart-container flex flex-col align-center justify-center">
-					<h2>Features Extraction</h2>
-					<PieChartComp data={pieData3}></PieChartComp>
-				</div>
+				{tasksTypeCount && (
+					<div className="piechart-container flex flex-col align-center justify-center">
+						<h2>Tasks Usage</h2>
+						<PieChartComp data={tasksTypeCount}></PieChartComp>
+					</div>
+				)}
+				{modelTypeCount && (
+					<div className="piechart-container flex flex-col align-center justify-center">
+						<h2>Models Usage</h2>
+						<PieChartComp data={modelTypeCount}></PieChartComp>
+					</div>
+				)}
+				{featuresCount && (
+					<div className="piechart-container flex flex-col align-center justify-center">
+						<h2>Features Extraction</h2>
+						<PieChartComp data={featuresCount}></PieChartComp>
+					</div>
+				)}
 			</div>
 			{tasksWeekCount && (
 				<div className="linecharts-container flex justify-center align-center flex-wrap padding-s">
