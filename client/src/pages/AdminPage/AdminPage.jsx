51c6f7fdd6bb110971c0fb4a756f4b770051433b
Mohamad-Rida-Chouman <shumanmohammadrida@gmail.com>
2023-09-27 14:06:52 +0300

    <FE> Modified Titles/Data (Admin Page)

--- client/src/pages/AdminPage/AdminPage.jsx
+++ client/src/pages/AdminPage/AdminPage.jsx
@@ -3,14 +3,28 @@ import './AdminPage.css';
 import '../../base.css';
 import LineChartComp from '../../components/LineChartComp/LineChartComp';
 import PageTitle from '../../components/PageTitle/PageTitle';
+import Button from '../../components/Button/Button';
 
 const AdminPage = () => {
 	const pieData1 = [
-		{ name: 'name1', value: 400 },
-		{ name: 'name2', value: 700 },
-		{ name: 'name3', value: 200 },
-		{ name: 'name4', value: 1000 },
+		{ name: 'Data Preprocessing', value: 400 },
+		{ name: 'Feature Extraction', value: 700 },
+		{ name: 'Model Run', value: 200 },
 	];
+	const pieData2 = [
+		{ name: 'LGB', value: 400 },
+		{ name: 'XGB', value: 300 },
+		{ name: 'RF', value: 50 },
+		{ name: 'Ensemble', value: 500 },
+	];
+	const pieData3 = [
+		{ name: 'AAC', value: 400 },
+		{ name: 'AAI', value: 700 },
+		{ name: 'Binary', value: 200 },
+		{ name: 'QSO', value: 1000 },
+		{ name: 'APAAC', value: 1000 },
+	];
+
 	const lineData1 = [
 		{ name: 'Monday', value: 11 },
 		{ name: 'Tuesday', value: 15 },
@@ -21,22 +35,29 @@ const AdminPage = () => {
 		{ name: 'Sunday', value: 0 },
 	];
 	return (
-		<div className="admin-main-container flex flex-col gap-l padding-s">
-			<div className="piecharts-container flex justify-center align-center flex-wrap gap-s">
-				<div className="piechart-container">
+		<div className="admin-main-container flex flex-col gap-l padding-s width-90">
+			<div className="flex">
+				<PageTitle title="Admin Dashboard"></PageTitle>
+				<Button className="button-dropdown button-s justify-center flex">
+					Logout
+				</Button>
+			</div>
+			<div className="piecharts-container flex justify-between align-center flex-wrap gap-s">
+				<div className="piechart-container flex flex-col align-center justify-center">
+					<h2>Tasks Usage</h2>
 					<PieChartComp data={pieData1}></PieChartComp>
 				</div>
-				<div className="piechart-container">
-					<PieChartComp data={pieData1}></PieChartComp>
+				<div className="piechart-container flex flex-col align-center justify-center">
+					<h2>Models Usage</h2>
+					<PieChartComp data={pieData2}></PieChartComp>
 				</div>
-				<div className="piechart-container">
-					<PieChartComp data={pieData1}></PieChartComp>
-				</div>
-				<div className="piechart-container">
-					<PieChartComp data={pieData1}></PieChartComp>
+				<div className="piechart-container flex flex-col align-center justify-center">
+					<h2>Features Extraction</h2>
+					<PieChartComp data={pieData3}></PieChartComp>
 				</div>
 			</div>
 			<div className="linecharts-container flex justify-center align-center flex-wrap padding-s">
+				<h2>Recent Week Tasks Count</h2>
 				<LineChartComp data={lineData1}></LineChartComp>
 			</div>
 		</div>
