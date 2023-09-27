ae980b76fcdb1ceaf22ea1655ec8d0268f721a5f
Mohamad-Rida-Chouman <shumanmohammadrida@gmail.com>
2023-09-27 12:37:33 +0300

    <FE> Created Piechart Component

---
+++ client/src/components/PieChartComp/PieChartComp.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { PieChart, Pie, Cell, Legend } from 'recharts';
+
+const PieChartComp = (props) => {
+	const COLORS = ['#8884d8', '#82ca9d', '#FFBB28', '#FF8042', '#AF19FF'];
+	const pieData = props.data;
+	return (
+		<PieChart className="piechart" width={350} height={350}>
+			<Pie
+				data={pieData}
+				color="#000000"
+				dataKey="value"
+				nameKey="name"
+				// outerRadius={120}
+				fill="green"
+				label
+			>
+				{pieData.map((entry, index) => (
+					<Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
+				))}
+			</Pie>
+			<Legend />
+		</PieChart>
+	);
+};
+
+export default PieChartComp;
