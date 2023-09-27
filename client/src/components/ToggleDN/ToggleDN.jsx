bd1f32a6eb0737da53c270e029f9c80a5ca1402a
Mohamad-Rida-Chouman <shumanmohammadrida@gmail.com>
2023-09-27 18:59:16 +0300

    <FE> Created Day-Night Toggle Button Component

---
+++ client/src/components/ToggleDN/ToggleDN.jsx
@@ -0,0 +1,42 @@
+import './ToggleDN.css';
+const ToggleDN = () => {
+	return (
+		<div className="toggle-button">
+			<label className="bb8-toggle">
+				<input className="bb8-toggle__checkbox" type="checkbox" />
+				<div className="bb8-toggle__container">
+					<div className="bb8-toggle__scenery">
+						<div className="bb8-toggle__star"></div>
+						<div className="bb8-toggle__star"></div>
+						<div className="bb8-toggle__star"></div>
+						<div className="bb8-toggle__star"></div>
+						<div className="bb8-toggle__star"></div>
+						<div className="bb8-toggle__star"></div>
+						<div className="bb8-toggle__star"></div>
+						<div className="tatto-1"></div>
+						<div className="tatto-2"></div>
+						<div className="gomrassen"></div>
+						<div className="hermes"></div>
+						<div className="chenini"></div>
+						<div className="bb8-toggle__cloud"></div>
+						<div className="bb8-toggle__cloud"></div>
+						<div className="bb8-toggle__cloud"></div>
+					</div>
+					<div className="bb8">
+						<div className="bb8__head-container">
+							<div className="bb8__antenna"></div>
+							<div className="bb8__antenna"></div>
+							<div className="bb8__head"></div>
+						</div>
+						<div className="bb8__body"></div>
+					</div>
+					<div className="artificial__hidden">
+						<div className="bb8__shadow"></div>
+					</div>
+				</div>
+			</label>
+		</div>
+	);
+};
+
+export default ToggleDN;
