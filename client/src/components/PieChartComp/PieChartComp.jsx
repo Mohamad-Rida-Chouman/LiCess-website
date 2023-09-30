7f37fb939a31c69c2bda6b0dbfcdb81e29769cda
Mohamad-Rida-Chouman <shumanmohammadrida@gmail.com>
2023-09-30 16:33:33 +0300

    <FE> Updated PieChart Style (Color, Legend, Hover)

--- client/src/components/PieChartComp/PieChartComp.jsx
+++ client/src/components/PieChartComp/PieChartComp.jsx
@@ -1,24 +1,32 @@
 import React from 'react';
-import { PieChart, Pie, Cell, Legend } from 'recharts';
+import { PieChart, Pie, Cell, Tooltip, Legend } from 'recharts';
+import CustomTooltip from '../CustomToolTip/CustomToolTip';
 
 const PieChartComp = (props) => {
-	const COLORS = ['#8884d8', '#82ca9d', '#FFBB28', '#FF8042', '#AF19FF'];
+	const COLORS = ['#E0DEFF', '#BAEECD', '#FFDF9B', '#FF9D9D', '#E5B5FF'];
 	const pieData = props.data;
+
 	return (
 		<PieChart className="piechart" width={250} height={250}>
+			<Legend
+				layout="vertical"
+				align="left"
+				verticalAlign="middle"
+				wrapperStyle={{ width: 100, whiteSpace: 'break-spaces' }}
+			/>
 			<Pie
 				data={pieData}
 				color="#000000"
 				dataKey="value"
 				nameKey="name"
 				fill="green"
-				label
+				// label
 			>
 				{pieData.map((entry, index) => (
 					<Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
 				))}
 			</Pie>
-			<Legend />
+			<Tooltip content={<CustomTooltip />} />
 		</PieChart>
 	);
 };
